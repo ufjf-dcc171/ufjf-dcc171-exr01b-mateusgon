bfbee72be06c3fdcc3d2c48c53b55e9fafcd6c3e
Mateus G <Mateus G@192.168.1.4>
2017-08-12 16:07:08 -0300

    Colocando Try e Catch

--- src/dcc171/ufjf/br/Aula02Exr01b.java
+++ src/dcc171/ufjf/br/Aula02Exr01b.java
@@ -5,6 +5,7 @@ import javax.swing.JOptionPane;
 public class Aula02Exr01b {
 
     public static void main(String[] args) {
+        try {
         Double x1, x2;
         Double y1, y2;
         String x = JOptionPane.showInputDialog("Digite o valor de X1");
@@ -18,6 +19,10 @@ public class Aula02Exr01b {
         Double valor = Math.sqrt((Math.pow((x2-x1), 2)) + (Math.pow(y2-y1, 2)));
         JOptionPane.showMessageDialog(null, "A distância entre os pontos X1 (" +  
         x1 + "," + y1 + ") e X2 (" + x2 + "," + y2 + ") +  é " + valor, "Resultado Final", JOptionPane.INFORMATION_MESSAGE);
+        }
+        catch (NumberFormatException ex)
+        {
+            JOptionPane.showMessageDialog(null,"Algum valor foi digitado de forma errada", "Digite novamente", JOptionPane.ERROR_MESSAGE);
+        }
     }
-    
 }
