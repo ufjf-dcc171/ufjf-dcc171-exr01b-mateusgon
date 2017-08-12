258d74b72122dda2d647b72c2dbdf1978ff51472
Mateus G <Mateus G@192.168.1.4>
2017-08-12 15:51:40 -0300

    Criando variáveis e criando caixas de diálogo

---
+++ src/dcc171/ufjf/br/Aula02Exr01b.java
@@ -0,0 +1,16 @@
+package dcc171.ufjf.br;
+
+import javax.swing.JOptionPane;
+
+public class Aula02Exr01b {
+
+    public static void main(String[] args) {
+        Double x1, x2;
+        Double y1, y2;
+        String x = JOptionPane.showInputDialog("Digite o valor de X1");
+        String y = JOptionPane.showInputDialog("Digite o valor de Y1");
+        String xi = JOptionPane.showInputDialog("Digite o valor de X2");
+        String yi = JOptionPane.showInputDialog("Digite o valor de Y2");
+    }
+    
+}
