a504b28fa2732a11f7aa210e894395416ca49321
Mateus G <Mateus G@192.168.1.4>
2017-08-12 16:03:01 -0300

    Gerando o resultado e imprimindo na caixa de diálogo

--- src/dcc171/ufjf/br/Aula02Exr01b.java
+++ src/dcc171/ufjf/br/Aula02Exr01b.java
@@ -11,6 +11,13 @@ public class Aula02Exr01b {
         String y = JOptionPane.showInputDialog("Digite o valor de Y1");
         String xi = JOptionPane.showInputDialog("Digite o valor de X2");
         String yi = JOptionPane.showInputDialog("Digite o valor de Y2");
+        x1 = Double.parseDouble(x);
+        y1 = Double.parseDouble(y);
+        x2 = Double.parseDouble(xi);
+        y2 = Double.parseDouble(yi);
+        Double valor = Math.sqrt((Math.pow((x2-x1), 2)) + (Math.pow(y2-y1, 2)));
+        JOptionPane.showMessageDialog(null, "A distância entre os pontos X1 (" +  
+        x1 + "," + y1 + ") e X2 (" + x2 + "," + y2 + ") +  é " + valor, "Resultado Final", JOptionPane.INFORMATION_MESSAGE);
     }
     
 }
